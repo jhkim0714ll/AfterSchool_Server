39ae85750fbb8163e87bad829daeab67dfc4a29b
kimjunho <junho07140714@dgsw.hs.kr>
2023-03-14 20:32:15 +0900

    :tada: :: Initial commit

---
+++ src/main/java/kr/pe/afterschool/AfterSchoolServerApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.afterschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterSchoolServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterSchoolServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pe/afterschool/AfterSchoolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.afterschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterSchoolServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
