d2069fe99d0492c03508a89c86b27b315e29140b
kimjunho <junho07140714@dgsw.hs.kr>
2023-05-03 14:11:07 +0900

    :memo: :: add validate exception

--- src/main/java/kr/pe/afterschool/global/error/handler/AfterSchoolExceptionHandler.java
+++ src/main/java/kr/pe/afterschool/global/error/handler/AfterSchoolExceptionHandler.java
@@ -1,9 +1,11 @@
 package kr.pe.afterschool.global.error.handler;
 
 import kr.pe.afterschool.global.error.AfterSchoolException;
+import kr.pe.afterschool.global.response.Response;
 import kr.pe.afterschool.global.response.ResponseError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,12 +13,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AfterSchoolExceptionHandler {
 
     @ExceptionHandler(AfterSchoolException.class)
-    protected ResponseEntity<ResponseError> handleAfterSchoolException(AfterSchoolException e) {
+    protected ResponseEntity<ResponseError> handleAfterSchoolExceptions(AfterSchoolException e) {
         final ResponseError responseError = ResponseError.builder()
                 .status(HttpStatus.valueOf(e.getErrorProperty().getStatus()))
                 .message(e.getErrorProperty().getMessage())
                 .build();
         return ResponseEntity.status(e.getErrorProperty().getStatus()).body(responseError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ResponseError> handleValidateExceptions(MethodArgumentNotValidException e) {
+        final ResponseError responseError = ResponseError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("누락된 값이 존재합니다")
+                .build();
+        return ResponseEntity.status(400).body(responseError);
     }
 
     @ExceptionHandler(Exception.class)
--- src/main/java/kr/pe/afterschool/global/security/SecurityConfiguration.java
+++ src/main/java/kr/pe/afterschool/global/security/SecurityConfiguration.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +37,8 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .cors().disable()
+                .cors();
+        http
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
@@ -57,5 +63,19 @@ public class SecurityConfiguration {
     @Bean
     protected PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration  = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
